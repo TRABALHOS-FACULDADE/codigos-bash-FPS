282281de859fd873cde687f193ca0634f2d51e9d
Felipe Ribeiro <feliper.dev@gmail.com>
2024-06-04 21:02:06 -0300

    Using dialog

--- TERCEIRA UNIDADE/extsite.sh
+++ TERCEIRA UNIDADE/extsite.sh
@@ -4,38 +4,55 @@
 ### Discente: Felipe Azevedo Ribeiro
 ### Docente: José Marivaldo de Aragão Silva
 
-echo "Bem vindo(a)s ao portal PokéMothim CLI!"
-echo "Deseja obter informações relacionadas a qual tema do mundo Pokémon?"
-echo ""
-echo "[1] Pokémon GO"
-echo "[2] Pokémon Unite"
-echo "[3] Pokémon Sleep"
-echo "[4] Pokémon Scarlet & Violet"
-echo "[5] Pokémon (Anime)"
-echo ""
+version="1.0.0"
 
-read OPCAO
+dialog  --msgbox   "Bem vindo(a)s ao portal PokéMothim CLI!\nDesenvolvido por: Felipe Azevedo Ribeiro\nVersão: $version" 10 70
+
+decision=$(dialog --menu "Deseja acessar o portal?" 12 45 25 1 "Sim" 2 "Não" --output-fd 1)
+
+if [ "$decision" = 2 ]; then
+    clear
+    exit
+else
+    clear
+fi
+
+titulos=(
+    "Pokémon GO"
+    "Pokémon (Anime)"
+    "Análise de Pokémon"
+)
+
+temas=(
+    "pokemon-go"
+    "anime"
+    "análise"
+)
+
+option=$(dialog --menu "Deseja obter informações e notícias relacionadas a qual tema do mundo Pokémon?" 12 45 25 1 "Pokémon GO" 2 "Pokémon (Anime)" 3 "Análise de Pokémon" --output-fd 1)
+
+clear
+
+opcaoSelecionada="${titulos[$(($option-1))]}"
+keySelecionada="${temas[$(($option-1))]}"
 
 base_url="https://www.pokemothim.net"
 
-# Temas para extrair dados
-temas=(
-    "pokemon-go"
-    "pokemon-unite"
-    "pokemon-sleep"
-    "pokemon-scarlet-violet"
-    "anime"
-)
+echo ""
+echo "Buscando informações relacionadas a $opcaoSelecionada"
+echo "Aguarde..."
+echo ""
 
 extrair_dados() {
-    tema="$1"
-    arquivo_saida="pokemoth_$tema.txt"
+    arg="$1"
+    tema="${arg//[á]/a}"
+    arquivo_saida="pokemothim_$tema.txt"
 
     # Acessar a página principal do tema
     lynx --listonly \
     --nonumbers \
     --display_charset=utf-8 \
-    --dump "$base_url"/search?q="$tema" | sed 's/[0-9]*\. //g' | grep "$tema" > temp.txt
+    --dump "$base_url"/search?q="$tema" | sed 's/[0-9]*\. //g' | sed '/#/d' | grep "$tema" > temp.txt
 
     # Extrair dados de cada link da página principal
     while read -r url; do
@@ -43,7 +60,10 @@ extrair_dados() {
     done < temp.txt
 
     rm temp.txt
+
+    dialog --msgbox  "Arquivo de texto com informações sobre $opcaoSelecionada foi gerado com sucesso!" 10 25
+    clear
 }
 
-opt=(OPCAO-1)
-extrair_dados "${temas[$opt]}"
+# Execução principal
+extrair_dados "$keySelecionada"
